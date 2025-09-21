9a32ae4e4907d93a6a648e24658f80508a87edae
Styles-Theandriod <emmanuelajah188@gmail.com>
2025-09-21 19:01:28 +0100

    added axios

---
+++ app/api/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const API = axios.create({
+  baseURL: "http://192.168.1.20:5000/api/auth", // replace with your local IP
+});
+
+// attach token automatically
+API.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export const registerUser = (data) => API.post("/register", data);
+export const loginUser = (data) => API.post("/login", data);
+export const forgotPassword = (data) => API.post("/forgot-password", data);
+export const googleLogin = (data) => API.post("/google", data);
